e22d805e37870a47b05e0c20302c726e4298ac24
saravanan <saravananr6332@gmail.com>
2018-11-24 19:45:52 +0530

    completed the project

---
+++ js/mapbox.js
@@ -0,0 +1,6 @@
+class MAPBOX {
+    static get mapbox_key() {
+      return 'pk.eyJ1Ijoic2FyYTE5OThyIiwiYSI6ImNqb3RucHdsYTB6djkzcHBhaW5xZGtjZWMifQ.wGqkFGC8ApYkrBjZkrBuog';
+           // 
+    }
+  }
